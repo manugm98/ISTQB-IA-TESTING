96a0d6715b1c1ec9428fdadbba090991928e85c6
Manuela Garcia <manuela.garcia@manuelagarcia.local>
2025-08-11 17:05:03 -0500

    fix: configuración Tailwind y PostCSS

--- ctai-simulador/vite.config.js
+++ ctai-simulador/vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// Detecta si estamos en producción (GitHub Pages, Netlify, etc.)
 const isProd = process.env.NODE_ENV === 'production'
 
 export default defineConfig({
-  base: isProd ? '/ISTQB-IA-TESTING/' : '/', // 👈 usa subcarpeta solo en producción
+  base: isProd ? '/ISTQB-IA-TESTING/' : '/',
   plugins: [
     react(),
     tailwindcss()
